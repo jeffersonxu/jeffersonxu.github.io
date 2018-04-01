10de23433bdcd9ba11baa486513f9994bfc33f46
jeffersonxu <me@jeffersonxu.com>
2018-04-01 15:10:00 -0400

    JS Cleanup

--- js/home.js
+++ js/home.js
@@ -110,8 +110,6 @@ $(document).ready(function(){
 	    $('#home-section').height(winHeight);
 	}
 
-	runTyping();
-
 	if (!mobile) {
 		mouseMoveMatrix();
 	} else {
@@ -159,62 +157,6 @@ $(document).ready(function(){
 	        offset: '60%'
 	    });
 	});
-
-	function runTyping() {
-		var element = document.querySelector('#output');
-		var typeSpeed = 100; // 80 ms
-		var deleteSpeed = 30; // 30 ms
-		var deleteAfter = 1000; // 1 second
-		var items = [
-		  "I create cool websites",
-		  "I build simple apps",
-		  "I am a ukulele and piano enthusiast",
-		  "I wish unicorns existed",
-		  "Imagination + Creativity = Design"
-		];
-		/* END SETTINGS */
-
-		var sentence = 0;
-		var currentChar = 0;
-		var direction = 1;
-		var deleteInterval = null;
-
-		function type() {
-		  if (sentence >= items.length) {
-		    sentence = 0;
-		  }
-
-		  var chars = items[sentence].split("");
-
-		  setTimeout(function() {
-		    if (currentChar >= chars.length) {
-		      setTimeout(function() {
-		        sentence++;
-
-		        deleteInterval = setInterval(function() {
-		        	element.innerHTML = element.innerHTML.substr(0, currentChar - 1);
-		          	
-		          currentChar--;
-
-		          if (currentChar == 1) {
-		            clearInterval(deleteInterval);
-		            type();
-		          }
-		        }, deleteSpeed);
-		      }, deleteAfter);
-
-		      return;
-		    }
-
-		    element.innerHTML += chars[currentChar];
-		    currentChar++;
-
-		    type();
-		  }, typeSpeed);
-		}
-
-		type();
-	}
 
 	function mouseMoveMatrix() {
 		$('.tile')
